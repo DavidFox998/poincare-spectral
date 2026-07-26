86a59acc21aebd41e0863dba5ce8d63054c56a55
DAVID J FOX <davidjfox998@gmail.com>
2026-07-25 23:21:52 -0700

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -1,15 +1,19 @@
 import Lake
 open Lake DSL
 
-package poincareSpectral where
+package poincare_spectral where
   version := v!"0.1.0"
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.12.0"
 
 @[default_target]
-lean_lib PoincareSpectral where
-  srcDir := "."
+lean_lib Towers where
+  srcDir := "Towers"
 
-lean_exe poincareSpectral where
+@[default_target]
+lean_lib Spectral where
+  srcDir := "Spectral"
+
+lean_exe main where
   root := `Main
