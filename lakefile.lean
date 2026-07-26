0b7d569c1ac232d8b782897bd8f06a76b4dd2bdf
DAVID J FOX <davidjfox998@gmail.com>
2026-07-25 23:43:20 -0700

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,9 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.12.0"
 
 @[default_target]
-lean_lib Poincare where
+lean_lib Lean where
   srcDir := "."
-  globs := #[.submodules `Towers, .submodules `Spectral]
+
+@[default_target]
+lean_lib Towers where
+  srcDir := "."
