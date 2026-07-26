4d0a53915ed4656be002f3bb1754a654ca17f90f
DAVID J FOX <davidjfox998@gmail.com>
2026-07-26 00:44:13 -0700

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -8,7 +8,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.12.0"
 
 @[default_target]
-lean_lib Lean where
+lean_lib core where
   srcDir := "."
 
 @[default_target]
